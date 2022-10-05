cd7f391255c4442a5367722d20fee218893146ee
Hassan Ali <17021308@student.hhs.nl>
2022-10-05 23:09:35 +0200

    changed vector app map input

--- apps/vector.py
+++ apps/vector.py
@@ -5,10 +5,17 @@ import leafmap.foliumap as leafmap
 def app():
 
     st.title("Vector")
-    
-    in_csv = 'https://raw.githubusercontent.com/giswqs/data/main/world/world_cities.csv'  
 
-    m = leafmap.Map(tiles="stamentoner")
-    m.add_xy_data(in_csv, x="longitude", y="latitude", layer_name="World Cities")
+    m = leafmap.Map(center=[0, 0], zoom=2)
+
+
+    in_geojson = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/cable_geo.geojson'
+    m.add_geojson(in_geojson, layer_name="Cable lines")
+
+
+    #in_csv = 'https://raw.githubusercontent.com/giswqs/data/main/world/world_cities.csv'  
+
+    #m = leafmap.Map(tiles="stamentoner")
+    #m.add_xy_data(in_csv, x="longitude", y="latitude", layer_name="World Cities")
     
     m.to_streamlit(height=700)
