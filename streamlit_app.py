5b8c8df83d87a3568e582b270010588b3c453182
Hassan Ali <17021308@student.hhs.nl>
2022-10-05 15:18:14 +0200

    Testing git_hub

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 
 # A dictionary of apps in the format of {"App title": "App icon"}
 # More icons can be found here: https://icons.getbootstrap.com
+#test
 
 apps = [
     {"func": home.app, "title": "Home", "icon": "house"},
